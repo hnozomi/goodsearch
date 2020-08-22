4db09e6837f27730773769062feac980777970b1
nozomi <ak62_hmkt_espoir@yahoo.co.jp>
2020-08-22 13:41:07 +0900

    '0822修正前'

--- src/components/App.js
+++ src/components/App.js
@@ -19,25 +19,42 @@ class App extends React.Component {
     }
     this.getTwitterData = this.getTwitterData.bind(this)
     this.clickSearchButton = this.clickSearchButton.bind(this)
+    // this.getUserId = this.getUserId(this)
   }
 
-  async clickSearchButton() {
+  async clickSearchButton(userID) {
+    console.log(userID, '検索開始')
+
+
+
+    if (isNaN(userID)) {
+      var param = {
+        searchWord: this.state.searchWord,
+        searchRadio: this.state.radio
+      }
+    } else {
+      var param = {
+        userID: userID,
+        searchRadio: 'account'
+      }
+    }
+
     this.getLoadingFalse()
 
-    await this.getTwitterData()
+    await this.getTwitterData(param)
 
     this.getLoadingTrue()
 
   }
 
-  async getTwitterData() {
+  async getTwitterData(param) {
     // const URL = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=nozo03234869&count=1"
     const URL = "https://k7v16hvrjj.execute-api.ap-northeast-1.amazonaws.com/lambda_test"
     const token = "AAAAAAAAAAAAAAAAAAAAAKhcGwEAAAAAfzmCghSvLicEU5NdtwA%2FC39bzSo%3DbHjdNmI4AqnECh8bshmXrfFGRIshe3MLjga8NsatzdQvR1D7dc"
-    const param = {
-      searchWord: this.state.searchWord,
-      searchRadio: this.state.radio
-    }
+    // const param = {
+    //   searchWord: this.state.searchWord,
+    //   searchRadio: this.state.radio
+    // }
 
     await axios.get(URL, { params: param }).then(response => {
       console.log(response.config.params.searchRadio)
@@ -54,6 +71,10 @@ class App extends React.Component {
       )
   }
 
+  // ****************************************************************///
+  // TwitterAPIから返答がくるのを待つ
+  // ****************************************************************///
+
   getLoadingFalse() {
     this.setState({
       loading: false
@@ -66,6 +87,10 @@ class App extends React.Component {
     })
   }
 
+  // ****************************************************************///
+  // 検索文字
+  // ****************************************************************///
+
   filterSearchWord(e) {
     const value = e.target.value;
     this.setState({
@@ -73,11 +98,24 @@ class App extends React.Component {
     })
   }
 
+  // ****************************************************************///
+  // 選択されるラジオボタンを取得
+  // ****************************************************************///
+
   handleRadioButton(event) {
     this.setState({
       radio: event
     })
   }
+
+
+
+  getUserId(userId) {
+    console.log(userId, this.state)
+  }
+
+
+
 
   render() {
     console.log(this.state.tweetData)
@@ -88,31 +126,46 @@ class App extends React.Component {
           いいね検索
       </header>
         <div className="search-form-wrapper">
-          <form className="search-form">
+          {/* <form className="search-form">
             <input value={this.state.searchWord} className="search-input input-area" onChange={e => this.filterSearchWord(e)} placeholder="検索"></input>
-          </form>
+          </form> */}
 
-            <label><input type="radio" onChange={() => this.handleRadioButton("content")} value="content" name="check" defaultChecked ></input>内容</label>
-            <label><input type="radio" onChange={() => this.handleRadioButton("name")} value="name" name="check" ></input>名前</label>
-            <label><input type="radio" onChange={() => this.handleRadioButton("date")} value="date" name="check"></input>日付</label>
+          <div class="mt-4">
+            <span class="text-gray-700">検索方法</span>
+            <div class="mt-2">
+              <label class="inline-flex items-center">
+                <input type="radio" class="form-radio" name="check" value="content" onChange={() => this.handleRadioButton("content")} defaultChecked></input>
+                <span class="ml-2">本文</span>
+              </label>
+              <label class="inline-flex items-center ml-6">
+                <input type="radio" class="form-radio" name="check" value="name" onChange={() => this.handleRadioButton("name")}></input>
+                <span class="ml-2">アカウント</span>
+              </label>
+            </div>
+          </div>
 
-          {/* <form class="w-5/6 max-w-sm container mx-auto">
+          {/* <label><input type="radio" onChange={() => this.handleRadioButton("content")} value="content" name="check" defaultChecked ></input>内容</label>
+          <label><input type="radio" onChange={() => this.handleRadioButton("name")} value="name" name="check" ></input>名前</label>
+          <label><input type="radio" onChange={() => this.handleRadioButton("date")} value="date" name="check"></input>日付</label> */}
+
+          <form class="w-5/6 max-w-sm container mx-auto">
             <div class="flex items-center border-b border-teal-500 py-2">
-              <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="ここに入力" aria-label="Full name"></input>
+              <input value={this.state.searchWord} className="search-input input-area" onChange={e => this.filterSearchWord(e)} class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="ここに入力" aria-label="Full name"></input>
               <button onClick={this.clickSearchButton} class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
                 検索
               </button>
             </div>
-          </form> */}
+          </form>
 
         </div>
 
-        <button onClick={this.clickSearchButton}>データ取得</button>
+        {/* <button onClick={this.clickSearchButton}>データ取得</button> */}
 
         {this.state.loading
           ? <Display
             tweetData={this.state.tweetData}
             radio={this.state.radio}
+            clickSearchButton={this.clickSearchButton}
 
           />
           : <p>LOADING・・・</p>
--- src/components/Display.js
+++ src/components/Display.js
@@ -11,8 +11,14 @@ class Display extends React.Component {
             tweetData: this.props.tweetData,
             radio: this.props.radio
         }
-        console.log(this.props.tweetData)
     }
+
+
+    // getTest(userId) {
+    //     this.props.getUserId(userId)
+    // }
+
+
     render() {
 
         const values = [];
@@ -42,29 +48,17 @@ class Display extends React.Component {
 
                         // : (this.state.tweetData.map((tweet, tweet_key) => {
                         : (cutUser.map((tweet, tweet_key) => {
+                            console.log(tweet,'tweet')
                             return (
-                                // <div className="tweet-card" key={tweet_key}>
-                                //     <img className="tweet-icon" src={tweet.user.profile_image_url}></img>
-                                //     <p className="tweet-name">{tweet.user.name}</p>
-                                //     <p className="tweet-text">{tweet.user.description}</p>
-
-                                // </div>
-                                <div class="flex justify-center items-center h-screen"  key={tweet_key}>
+                                <div onClick={() => this.props.clickSearchButton(tweet.user.id)} class="flex justify-center items-center h-auto m-8"  key={tweet_key}>
                                     <div class="max-w-sm rounded overflow-hidden shadow-lg">
                                         {/* <img class="w-full" src={tweet.user.profile_image_url} alt="Sunset in the mountains"></img> */}
-                                        <img class="w-2/3 mx-14" src={tweet.user.profile_image_url} alt="Sunset in the mountains"></img>
+                                        <img class="w-10 m-auto" src={tweet.user.profile_image_url} alt="Sunset in the mountains"></img>
                                         <div class="px-6 py-4">
                                             <div class="font-bold text-xl mb-2">{tweet.user.name}</div>
                                             <p class="text-gray-700 text-base">
                                                 {tweet.user.description}
                                             </p>
-                                        </div>
-                                        <div class="px-6 py-4">
-                                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                                                #photography
-                                            </span>
-                                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-                                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
                                         </div>
                                     </div>
                                 </div>
